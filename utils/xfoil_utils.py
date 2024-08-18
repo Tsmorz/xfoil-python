2dd2c416c2a61ead3b80081c7269461f9bea32d1
Tsmorz <tonysmoragiewicz@gmail.com>
2024-08-17 20:06:47 -0400

    run xfoil at set aoa

--- utils/xfoil_utils.py
+++ utils/xfoil_utils.py
@@ -1,13 +1,27 @@
 from xfoil import XFoil
-from typing import List
+from typing import Any
+from xfoil.model import Airfoil
 
 
-def run_xfoil(airfoil, alpha: float, reynolds_number: float, max_iter: int) -> List:
+def run_xfoil(
+    airfoil: Airfoil, alpha: float, reynolds_number: float, max_iter: int = 50
+) -> Any:
+    """
+    Run XFoil with given airfoil, angle of attack, reynolds number, and maximum iterations.
+
+    Args:
+    airfoil (Airfoil): Airfoil object.
+    alpha (float): Angle of attack in degrees.
+    reynolds_number (float): Reynolds number.
+    max_iter (int): Maximum number of iterations.
+
+    Returns: simulation results from XFoil
+
+    """
     xf = XFoil()
     xf.airfoil = airfoil
     xf.Re = reynolds_number
+    xf.n_crit = 9
     xf.max_iter = max_iter
-
-    a, cl, cd, cm, cp = xf.aseq(alpha, alpha + 1, 1)
 
     return xf.aseq(alpha, alpha + 1, 1)
